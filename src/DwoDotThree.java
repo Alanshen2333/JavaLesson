8d9507988798973cdcd97f1e88267fcd6068d6b1
Alanshen233 <alanshen233@outlook.com>
2023-10-15 21:42:00 +0800

    Init

---
+++ src/DwoDotThree.java
@@ -0,0 +1,21 @@
+public class DwoDotThree {
+    public static void main(String[] args) {
+        double a1,b1,c1;
+        System.out.println("浮点型：");
+        a1=5/6;
+        System.out.println("5/6="+a1);
+        b1=5.0/6.0;
+        System.out.println("5.0/6.0="+b1);
+        c1=5/6.0;
+        System.out.println("5/6.0="+c1);
+        //
+        System.out.println("整数型:");
+        //int a2,b2,c2;
+        double a2=  5/6;
+        System.out.println("5/6="+a2);
+        double b2=  (5.0/6.0);
+        System.out.println("5.0/6.0="+b2);
+        double c2=  (5/6.0);
+        System.out.println("5/6.0="+c2);
+    }
+}
