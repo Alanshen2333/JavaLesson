298492e91334d1206485abf76bf1942c8a91ccf8
Alanshen233 <alanshen233@outlook.com>
2023-10-18 17:05:47 +0800

    2.6fixed

--- src/MyScanner.java
+++ src/MyScanner.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//2.6
 public class MyScanner {
     public static void main(String[] arg){
         Scanner newScanner = new Scanner(System.in);
@@ -9,7 +9,11 @@ public class MyScanner {
         System.out.println("你输入的字符串是："+s);
         a= newScanner.nextInt();
         System.out.println("你输入的整数是："+a);
+        System.out.println("从键盘输入一串单精度浮点数");
+        b = newScanner.nextFloat();
+        System.out.println("你输入的单精度浮点数是："+b);
         System.out.print("从键盘输入一串浮点数：");
-        b= newScanner.nextFloat();
+        c= newScanner.nextFloat();
+        System.out.println("你输入的双精度浮点数是："+c);
     }
 }
