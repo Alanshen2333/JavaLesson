ceccb9712b9b252563f42c2a5678e02fbc58c548
Alanshen2333 <56188655+Alanshen2333@users.noreply.github.com>
2024-09-29 07:54:00 +0000

    12.4,ToDo

---
+++ src/ New/SetDemo.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        int[] a={2,5,9,13};
+        int[] b = {1,3,6,9,15,21};
+        HashSet<Integer> setA = new HashSet<Integer>();
+        HashSet<Integer> setB = new HashSet<Integer>();
+        for (int i = 0; i < b.length; i++) {
+            //TO-DO
+        }
+    }
+}
