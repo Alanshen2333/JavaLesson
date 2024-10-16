5ca5e787063d1315fbbd440356ebf38ff3412645
Alanshen233 <alanshen233@outlook.com>
2024-10-16 21:13:54 +0800

    代码格式化

--- src/ New/MapDemo.java
+++ src/ New/MapDemo.java
@@ -1,33 +1,30 @@
 import java.util.*;
 
-
-
 public class MapDemo {
-    public static void main(String[] args) {
-        String key,value;
-        String[] engilsh = {"Sun","Mon","Tues","Wed","Thur","Fri","Sat"};
-        String[] chinese = {"星期日","星期一","星期二","星期三","星期四","星期五","星期六"};
-        HashMap<String,String> hashMap = new HashMap<String,String>();
-        for (int i = 0; i < chinese.length; i++) {
-            hashMap.put(engilsh[i],chinese[i]);  
-        }
-        Set<String> keys = hashMap.keySet();
-        Iterator<String> it = keys.iterator();
-        while(it.hasNext()){
-            key = it.next();
-            value = hashMap.get(key);
-            System.out.print(key+" "+value+"\t");
-        }
-        System.out.println();
+	public static void main(String[] args) {
+		String key, value;
+		String[] engilsh = { "Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat" };
+		String[] chinese = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+		HashMap<String, String> hashMap = new HashMap<String, String>();
+		for (int i = 0; i < chinese.length; i++) {
+			hashMap.put(engilsh[i], chinese[i]);
+		}
+		Set<String> keys = hashMap.keySet();
+		Iterator<String> it = keys.iterator();
+		while (it.hasNext()) {
+			key = it.next();
+			value = hashMap.get(key);
+			System.out.print(key + " " + value + "\t");
+		}
+		System.out.println();
 
-        
-        TreeMap<String,String> treeMap = new TreeMap<String,String>(hashMap);
-        keys = treeMap.keySet();
-        it=keys.iterator();
-        while (it.hasNext()) {
-            key = it.next();
-            value = treeMap.get(key);
-            System.out.print(key+" "+value+"\t");
-        }
-    }
+		TreeMap<String, String> treeMap = new TreeMap<String, String>(hashMap);
+		keys = treeMap.keySet();
+		it = keys.iterator();
+		while (it.hasNext()) {
+			key = it.next();
+			value = treeMap.get(key);
+			System.out.print(key + " " + value + "\t");
+		}
+	}
 }
